1645a72f1cb15112ebf89fb56726007e08b4c6de
ZurisaddaiRJ <zuleyma.juarez_2002@hotmail.com>
2023-03-07 10:19:42 -0600

    Create app New York City

---
+++ app-newyorkC.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+sidebar = st.sidebar
+st.title('PASEOS EN BICLETA NYC')
+
+#--- LOGO ---#
+st.sidebar.image('credencial.jpg')
+st.text('ZURISADDAI REYES JUÁREZ')
+st.text('Correo Electrónico: zS20006762@estudiantes.uv.mx')
+
+DATE_COLUMN = 'started_at'
+DATA_URL = ('citibike-tripdata.csv')
+
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename({'start_lat': 'lat', 'start_lng': 'lon'}, axis=1, inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+data_load_state = st.text('Loading cicle nyc data...')
+data = load_data(500)
+data_load_state.text("Done! (using st.cache)")
+
+if st.sidebar.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
+
+if st.sidebar.checkbox('Recorridos por hora'):
+    st.subheader('Numero de recorridos por hora')
+
+    hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
+
+
+# Some number in the range 0-23
+hour_to_filter = st.sidebar.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
